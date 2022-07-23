fdb6bbcf9d82afb5f22a19d845426992d8ecf123
Joseph Lim <25356643+JosephPLim@users.noreply.github.com>
2022-07-23 01:05:37 -0500

    Update FishSqInitialize.sql
    Update of Tables to more thoroughly separate data accordingly.

--- FishSqInitialize.sql
+++ FishSqInitialize.sql
@@ -1,30 +1,56 @@
 CREATE DATABASE FishSq;
 
+--Initializes Names table, which contains unique identifying numbers for fish, linked to their common and scientific name
 CREATE TABLE Names (
 	COMMONNAME varchar(255), --common name of the fish
 	SCINAME varchar(255), --scientific name of the fish
-	FISHID int --ID associated with each fish species (ex. Ampiprion percula = 1, Pterapogon kaudernii = 2)
+	FISHID int --ID associated with each fish species (ex. Amphiprion percula = 1, Pterapogon kaudernii = 2)
+
+	PRIMARY KEY (FISHID)
 );
 
+--Initializes Providers, where each row contains information about a singular supplier
+CREATE TABLE SUPPLIER (
+	NAME varchar(255) --Common name of supplier (ex. Golden Ina)
+	SUPPLIERID int --Unique ID # of supplier
+
+	PRIMARY KEY (ID)
+
+);
+
+--Initializes shipments, where each row contains information about a singular shipment
+CREATE TABLE SHIPMENTS (
+	SHIPPINGID int --unique shipping number specifically for that shipment. Primary Key
+	ARRIVAL DATE --Date the shipment arrived
+	LOCATION varchar(255)--shipping location (ex. Bali Lombok)
+	SUPPLIERID int-- shipping supplier ID # (ex. Golden Ina)
+	BOXES int -- # of boxes that came with the shipment
+	STATUS ENUM(E, O, L) -- if the shipment came in Early (before arrival date), On time (on arrival date), or Late (delayed)
+	LOCAL ENUM (Y, N)  -- If the shipment was local (i.e. no freight charge, no box charge)
+
+	PRIMARY KEY (SHIPPINGID)
+);
+
+--Initializes boxes, where each row contains information about a singular box
 CREATE TABLE BOXES (
-	BOXID int --ID of the box, Primary Key
-	BOXPRICE double(255,2) --price of the box
-	FREIGHTPRICE double(255, 2) --price of the freight for the box
-	LOCATION varchar(255) --location of where the fish arrived from
-	ARRIVAL DATE --what day the box arrived
-	
+	BOXID int --unique box ID, Primary Key
+	SHIPPINGID int -- ID of the shipment the box came with
+	FREIGHT double(15,2) --the price it took to ship the box freight
+	SHIPPING double(15, 2) --the price it took to ship the box
+	BAGS int -- # of bags that came with the shipment
 
+	PRIMARY KEY(BOXID)
 );
 
+--initializes Arrivals, where each row contains information about a single arriving fish
 CREATE TABLE ARRIVALS (
-	BOXID int --ID of the box the fish came in
-	FISHID int --ID associated with each fish species. See: names
-	BASEPRICE double(255, 2) --the base price of the fish (what you see on transship lists)
-	BAGID int --ID of the bag the fish came in, primary key
-	CIRCUMFERNCE int --circumference of the bag
-	HEIGHT int --height of the bag
-	WEIGHT int --weight of the bag
-	DEAD ENUM(Yes, No) --Is the fish dead?
+	BAGID int -- Unique ID of the bag
+	FISHID int -- ID associated with the fish species
+	BASE double(15,2) -- the base price of the fish
+	CIRCUMFERENCE int -- Circumference of the bag the fish was in
+	HEIGHT int --Height of the bag the fish was in
+	DEAD ENUM(Y, N) -- If the fish was (Y)es dead, or N(o) not dead
+
 
 	PRIMARY KEY (BAGID)
 );
